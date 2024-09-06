832a21107fbf89ff29f2fa5cee827c659b48c892
Yasser Taha <yasser1119992017@gmail.com>
2024-09-06 18:28:06 +0300

    down load angluer project and add bootstrap to it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StudentsComponent } from './components/day3/students/students.component';
+import { RegisterComponent } from './components/day3/register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,StudentsComponent,RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
